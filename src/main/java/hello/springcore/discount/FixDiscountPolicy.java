5b9d55475e8ac3b8c164a216b746309591d7a7c1
daeyounggu <ocstar9@gmail.com>
2023-05-15 16:49:40 +0900

    할인정책 인터페이스 구현체로  정액할인 클래스추가

--- src/main/java/hello/springcore/discount/FixDiscountPolicy.java
+++ src/main/java/hello/springcore/discount/FixDiscountPolicy.java
@@ -1,2 +1,18 @@
-package hello.springcore.discount;public class FixDiscountPolicy {
+package hello.springcore.discount;
+
+import hello.springcore.member.Grade;
+import hello.springcore.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; //1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return discountFixAmount;
+        }else{
+            return 0;
+        }
+    }
 }
