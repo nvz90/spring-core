3ea47597b4daa26406e60d4e83ea7c1d91aa4a1e
daeyounggu <ocstar9@gmail.com>
2023-05-17 19:34:59 +0900

    스프링 컨테이너 적용

--- src/main/java/hello/springcore/OrderApp.java
+++ src/main/java/hello/springcore/OrderApp.java
@@ -7,13 +7,19 @@ import hello.springcore.member.MemberServiceImpl;
 import hello.springcore.order.Order;
 import hello.springcore.order.OrderService;
 import hello.springcore.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
