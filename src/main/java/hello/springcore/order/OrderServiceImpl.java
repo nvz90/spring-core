ce8a9473d920938d4528bc7360490b809226ea95
daeyounggu <ocstar9@gmail.com>
2023-06-09 23:36:48 +0900

    롬복 RequiredArgsConstructor 어노태이션 적용

--- src/main/java/hello/springcore/order/OrderServiceImpl.java
+++ src/main/java/hello/springcore/order/OrderServiceImpl.java
@@ -5,19 +5,15 @@ import hello.springcore.discount.FixDiscountPolicy;
 import hello.springcore.member.Member;
 import hello.springcore.member.MemberRepository;
 import hello.springcore.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
