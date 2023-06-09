0ef3968cedd4591a59a38ffb6bca35bb5cc90158
daeyounggu <ocstar9@gmail.com>
2023-06-09 22:38:59 +0900

    생성자 주입과 수정자 주입 테스트 추가 생성자는 컴파일 시 오류 발생하는 반면 수정자는 런타임 때 예외가 터진다

--- src/test/java/hello/springcore/order/OrderServiceImplTest.java
+++ src/test/java/hello/springcore/order/OrderServiceImplTest.java
@@ -1,4 +1,25 @@
+package hello.springcore.order;
+
+import hello.springcore.discount.RateDiscountPolicy;
+import hello.springcore.member.Grade;
+import hello.springcore.member.Member;
+import hello.springcore.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class OrderServiceImplTest {
-  
+
+    @Test
+    void createOrder(){
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+        Member member = new Member(1L, "abac", Grade.VIP);
+        memoryMemberRepository.save(member);
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memoryMemberRepository, new RateDiscountPolicy());
+
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
 }
