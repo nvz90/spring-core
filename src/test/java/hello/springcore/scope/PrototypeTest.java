b81b9cf3bdc2602081abb97ca5f9aca7b58080f0
daeyounggu <ocstar9@gmail.com>
2023-06-12 00:28:29 +0900

    싱글톤 스코프 빈  테스트

---
+++ src/test/java/hello/springcore/scope/PrototypeTest.java
@@ -0,0 +1,2 @@
+package hello.springcore.scope;public class PrototypeTest {
+}
---
+++ src/test/java/hello/springcore/scope/SingletonTest.java
@@ -0,0 +1,41 @@
+package hello.springcore.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonTest.class);
+
+        SingletonTest singletonTest1 = ac.getBean(SingletonTest.class);
+        SingletonTest singletonTest2 = ac.getBean(SingletonTest.class);
+        System.out.println("singletonTest1 = " + singletonTest1);
+        System.out.println("singletonTest2 = " + singletonTest2);
+        assertThat(singletonTest1).isSameAs(singletonTest2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
+
